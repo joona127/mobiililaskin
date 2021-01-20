98488baf1e1645b0f24d35dfb062c0e2ef5b6713
joona127 <65039165+joona127@users.noreply.github.com>
2021-01-20 20:14:17 +0200

    laskin valmis

--- App.js
+++ App.js
@@ -1,21 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from 'react';
+ import { StyleSheet, View, TextInput, Text, Button, Alert} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  
+
+export default class Laskin extends Component {
+
+  //const [firstNumber, setFirstNumber] = useState('');
+  //const [secondNumber, setSecondNumber] = useState('');
+
+  constructor(props)
+  {
+    super(props)
+  this.state={firstNumber:0, secondNumber:0};
+  }
+
+  Sum = () => {
+    var N1 = parseInt(this.state.firstNumber);
+    var N2 = parseInt(this.state.secondNumber);
+
+    var result = N1+N2;
+    Alert.alert('Result is: ' + ' ' + result);
+    
+  }
+
+  Subtraction = () => {
+    var N1 = parseInt(this.state.firstNumber);
+    var N2 = parseInt(this.state.secondNumber);
+
+    var result = N1-N2;
+    Alert.alert('Result is: ' + ' ' + result);
+  }
+
+  render() 
+  {
+    return (
+
+ 
+ <View style={styles.container}>
+ 
+         <Text>Calculate: {this.Sum}</Text> 
+
+        <TextInput
+ 
+          placeholder="Number 1"
+ 
+          style={styles.TextInputStyle}
+ 
+          keyboardType={'numeric'}
+
+          onChangeText={firstNumber=>this.setState({firstNumber})}
+ 
+        />
+
+<TextInput
+ 
+ placeholder="Number 2"
+
+ style={styles.TextInputStyle}
+
+ keyboardType={'numeric'}
+
+          onChangeText={secondNumber=>this.setState({secondNumber})}
+
+/>
+
+
+
+<View style={styles.buttonContainer}>
+<View style={styles.button}>
+<Button title="+" onPress={this.Sum}/>
+</View>
+<View style={styles.button}>
+<Button title="-" onPress={this.Subtraction}/>
+</View>
+</View>
+</View>
+
+
+
+ 
+ 
+ 
+            
+    );
+  }
+
 }
-
-const styles = StyleSheet.create({
+ const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    flex:1,
+    margin: 10,
+    
   },
-});
+
+  TextInputStyle: {
+    
+    textAlign: 'center',
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    //justifyContent: 'space-between'
+
+  },
+  button: {
+    width: '18%',
+    height: 40,
+    //flexDirection: 'row',
+    //justifyContent: 'space-between',
+    padding: 15
+  }
+
+  });
+
